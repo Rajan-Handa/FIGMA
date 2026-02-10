2b0de5939ec244097f589da73678e5960b77a38a
Rajan <rajanhanda24@gmail.com>
2026-02-10 13:04:19 +0530

    fix jsx html error

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Features from './components/Features'
 import Programs from './components/Programs'
 import Campus from './components/Campus'
 import Scholarship from './components/Scholarship'
 import Footer from './components/Footer'
-import Hero from './components/Hero';
+import Hero from './components/Hero'
 import Stats from './components/Stats'
-import New from './components/New';
-import usePageTracking from './components/UsePageTracking'
+
 function App() {
-
-  usePageTracking();
-
   return (
-    <div>
-        <Hero/>
-        <Features/>
-        <Programs/>
-        <Stats/>
-        <Campus/>
-        <Scholarship/>
-        <Footer/>
-        <New/>
-    </div>
+    <BrowserRouter>
+      <Hero />
+      <Features />
+      <Programs />
+      <Stats />
+      <Campus />
+      <Scholarship />
+      <Footer />
+    </BrowserRouter>
   )
 }
 
